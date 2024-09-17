fc7456ad68c041289b89dfb24139472554a69632
Ngane Emmanuel <emmanuelngane06@gmail.com>
2024-09-17 13:54:02 -0700

    containerizing component

--- src/main/java/com/gethealthy/searchservice/model/SearchResult.java
+++ src/main/java/com/gethealthy/searchservice/model/SearchResult.java
@@ -13,10 +13,10 @@ public class SearchResult implements Comparable<SearchResult> {
     private Long id;
     private String title; //title of event or illness record (common property)
     private String description; //description of event or illness record (common property)
-    private SourceType source;  //from events or illness record services
     private String Status; //either the illness record status(started, ended) or the health status of the event
-    private EventType eventType; //if result is from the event service and what kind of event (Symptoms, hospital visit, medication administration)
     private String dateStarted; // started date (common property)
+    private SourceType source;  //from events or illness record services
+    private EventType eventType; //if result is from the event service and what kind of event (Symptoms, hospital visit, medication administration)
 
     @Override
     public int compareTo(SearchResult o) {
